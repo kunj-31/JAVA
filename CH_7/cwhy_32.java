2aed11a72d4ccd589747e5290bebd1b3a20bc8b5
kunj-31 <kunjdesai487@gmail.com>
2025-01-31 11:56:15 +0530

    Part:10

--- CH_7/cwhy_32.java
+++ CH_7/cwhy_32.java
@@ -1,37 +1,38 @@
 package CH_7;
 
+//method-overloading
 public class cwhy_32 {
-    static void food(){
+    static void food() {
         System.out.println("idli");
     }
-    static void food(int a){
-        System.out.println("dosa "+a+" vada");
-    }
-    static void food(int b,int c){//parameteres
-        System.out.println("nasta "+b+" our chai");
+
+    static void food(int a) {
+        System.out.println("dosa " + a + " vada");
     }
 
-    static void tellmeyourname(){
+    static void food(int b, int c) {// parameteres
+        System.out.println("nasta " + b + " our chai");
+    }
+
+    static void tellmeyourname() {
         System.out.println("------VEDDESAI--------");
     }
-    static void change(int []arr){
-        arr[0]=78;
+
+    static void change(int[] arr) {
+        arr[0] = 78;
     }
 
     public static void main(String[] args) {
         tellmeyourname();
-        int marks[]={56,85,88,89,78,123};
+        int marks[] = { 56, 85, 88, 89, 78, 123 };
         change(marks);
-        System.out.println("the value after the change is arr[0]:"+marks[0]);
-            
+        System.out.println("the value after the change is arr[0]:" + marks[0]);
 
-
-        //methodoverloading
+        // methodoverloading
         food();
         food(5);
-        food(6,7);
-        //actual arguments
+        food(6, 7);
+        // actual arguments
     }
 
 }
-
