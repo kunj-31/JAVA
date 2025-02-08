ded2dd6dc378df2d0518901fb585ccd7692570f1
kunj-31 <kunjdesai487@gmail.com>
2025-02-08 18:13:52 +0530

    Ch_14-2:thry_catch

---
+++ PRCTISESET/SET_14/ps_14.java
@@ -0,0 +1,73 @@
+// package PRCTISESET.SET_14;
+// import java.util.Scanner;
+
+// public class ps_14 {
+// public static void main(String[] args) {
+// problem-1
+/*
+ * syntax error
+ * int a=9;
+ * int b=7
+ * int c=a+b;
+ */
+
+// logical errors
+/*
+ * int age=78;
+ * int born_year=2000-78;
+ */
+
+// runtime errors
+// System.out.println(1/0);
+
+// --------------------------------------------------------//
+
+// problem-2
+
+// int c=9;
+// int b=7;
+
+// try{
+// System.out.println(c/b);
+// try{
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter the number:");
+// int n=sc.nextInt();
+// }
+// catch(Exception a){
+// System.out.println(a);
+// }
+// finally{
+// System.out.println("Hehe");
+// }
+// }
+// catch(Exception e){
+// System.out.println(e);
+// }
+// finally{
+// System.out.println("HaHa");
+// }
+
+// ---------------------------------------------------------------//
+// problem-3
+// int arr[]=new int[3];
+// arr[0]=12;
+// arr[1]=13;
+// arr[2]=14;
+// boolean flag=true;
+// Scanner sc=new Scanner(System.in);
+// int i=0;
+// int index;
+// while(flag && i<5){
+// try{
+// index=sc.nextInt();
+// System.out.println("the value of marks[index] is:"+arr[index]);
+// break;
+// }
+// catch(Exception e){
+// System.out.println("invailed Index");
+// i++;
+// }
+// }
+// }
+// }
